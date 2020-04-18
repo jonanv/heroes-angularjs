aef540985978f5997ef16643aaf02ccd2772982a
Giovanni Vargas <jonan-vargas23@hotmail.com>
2020-04-18 18:29:41 -0500

    Se instalo sweetAlert2 y se hizo mas eficiente el codigo cuando crea y actualiza

--- src/app/components/heroe/heroe.component.ts
+++ src/app/components/heroe/heroe.component.ts
@@ -6,6 +6,8 @@ import { HeroesService } from '../../services/heroes.service';
 
 // Imports
 import { first } from 'rxjs/operators';
+import Swal from "sweetalert2";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-heroe',
@@ -28,20 +30,35 @@ export class HeroeComponent implements OnInit {
       return;
     }
 
-    if(this.heroe.id) {
-      this.heroesService.updateHeroe(this.heroe)
-        .pipe(first())
-        .subscribe(response => {
-          console.log(response);
-        });
+    Swal.fire({
+      title: 'Espere',
+      text: 'Guardado informacion',
+      icon: 'info',
+      allowOutsideClick: false
+    });
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
+    let message: string;
+
+    if (this.heroe.id) {
+      peticion = this.heroesService.updateHeroe(this.heroe);
+      message = 'Se actualizo correctamente';
     }
     else {
-      this.heroesService.creatHeroe(this.heroe)
-        .pipe(first())
-        .subscribe(response => {
-          console.log(response);
-        });
+      peticion = this.heroesService.creatHeroe(this.heroe);
+      message = 'Se creo correctamente';
     }
+
+    peticion
+      .pipe(first())
+      .subscribe(response => {
+        Swal.fire({
+          title: this.heroe.name,
+          text: message,
+          icon: 'success'
+        });
+      });
 
   }
 
